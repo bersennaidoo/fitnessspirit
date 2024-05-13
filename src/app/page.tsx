656815fca631758d49dc5f852ec2048a036c4286
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-05-13 15:49:13 +0200

    Marked up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,25 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>Fitness Spirit Club</div>
+    <main>
+      <p>
+        Welcome to Fitness Spirit Club. Our mission is to help 
+        our clients meet their fitness and nutrition goals.
+      </p>
+
+      <p>
+        If you have struggled with getting healthy and need the motivation and 
+        resources to make a healthy lifestyle change, contact us today. Our facility 
+        includes state-of-the-art equipment, convenient group training classes, and 
+        nutrition tips and information to keep you healthy.
+      </p>
+
+      <p>
+        We provide a FREE one-week membership so you can experience the benefits 
+        of our equipment and facility. This one-week trial gives you complete access 
+        to our equipment, training classes, and nutrition planning. Contact us today 
+        to start your free trial!
+      </p>
+    </main>
   );
 }
