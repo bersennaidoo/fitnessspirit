f6f4deba54300a8f160249224c4364ee5829d15b
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-05-15 15:03:50 +0200

    Marked up contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,9 +19,7 @@ function ContactPage() {
         width="600"
         height="450"
         style={{border: 0}}
-        allowfullscreen=""
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
   );
